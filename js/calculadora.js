a3caf43381eb3e538480fbc09723bfb4cd608c38
Venicius <vidivinny8859@gmail.com>
2021-11-22 17:12:36 -0300

    removida a entrada dois, tentando criar com apenas uma entrada

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,10 +1,11 @@
 var painel = document.querySelector(".painel")
 var resultado = document.getElementById('resultado');
 var historico = document.getElementById('historico');
-var entrada1 = document.querySelector("#entrada1");
-var entrada2 = document.querySelector("#entrada2");
-var operadores = document.querySelector("#operadores");
-var operador = $('#operadores option:selected').text();
+var entrada = document.querySelector("#entrada1");
+// var entrada2 = document.querySelector("#entrada2");
+// var operadores = document.querySelector("#operadores");
+
+// var operacao = operadores.options[operadores.selectedIndex].text;
 
 const numeros = [0,1,2,3,4,5,6,7,8,9,',']
 
@@ -15,14 +16,20 @@ painel.addEventListener("keypress", function(e){
 });
 
 function calcular() {
-    var valor1 = entrada1.value;
-    var strn1 = valor1.replace(/,/, '.');
-    var n1 = Number(strn1);
-    var valor2 = entrada2.value;
-    var strn2 = valor2.replace(/,/, '.');
-    var n2 = Number(strn2);
+    var valor = entrada.value;
+    var strn = valor.replace(/,/, '.');
+    var n = Number(strn);
+
+    // INÍCIO - Cálculos
+   
+    
+    // FIM - Cálculos
+
+    var strRes = toSring(res);
+    var strn = strRes.replace(/./, ',');
+
     resultado.classList.add('text-primary');
     resultado.classList.remove('text-muted');
-    if (operadores.value)
-    resultado.innerHTML = `Entrada 1: ${n1}. Entrada 2: ${n2}. ${operador}`;
+    resultado.innerHTML = `${n}`;
+    
 }
