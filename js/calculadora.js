3a627a6fb0b80a96f071b3416dc93702e98da0eb
Venicius <vidivinny8859@gmail.com>
2021-12-03 15:48:16 -0300

    calculadora com select funcionando nas mudancas de operadores.

--- js/calculadora.js
+++ js/calculadora.js
@@ -4,41 +4,43 @@ var resultado = document.getElementById('resultado');
 var historico = document.getElementById('historico');
 var entrada1 = document.querySelector("#entrada1");
 var entrada2 = document.querySelector("#entrada2");
-var operador = document.querySelector("#operador")
 
-// var operacao = operadores.options[operadores.selectedIndex].text;
+// var operador = document.getElementById("sinal")
+var operadores = document.getElementById("operadores")
 
-const numeros = [0,1,2,3,4,5,6,7,8,9]
-const operadores = ['+','-','*','/']
+
+// operador.addEventListener("keypress", function(s) {
+//     const sinais = ['+','-','*','/'];
+//     if (sinais.find(element => element == s.key) !== undefined) {
+//         operador.value += s.key
+//     };
+// });
+
+operadores.addEventListener("change", calcular());
 
 painel.addEventListener("keypress", function(e) {
+    const numeros = [0,1,2,3,4,5,6,7,8,9];
     if (numeros.find(element => element == e.key) == undefined) {
         e.preventDefault();
-    }
+    };
     if (e.key == "," || e.key == ".") {
         if (entrada1.value.indexOf('.') == -1) {
             entrada1.value += '.';
-        }
-    }
+        };
+    };
     if (e.key == "," || e.key == ".") {
         if (entrada2.value.indexOf('.') == -1) {
             entrada2.value += '.';
-        }
-    }
-});
-
-operador.addEventListener("keypress", function(e) {
-    if (operadores.find(element => element == e.key) == undefined) {
-        e.preventDefault();
-    }
+        };
+    };
 });
 
 function calcular() {
     // var valor = entrada.value;
     // var n = parseFloat(valor);
-
+    var operacao = operadores.options[operadores.selectedIndex].value;
     resultado.classList.add('text-primary');
     resultado.classList.remove('text-muted');
-    resultado.innerHTML = `Funcionando!`;
+    resultado.innerHTML = `${operacao} Funcionando!`;
     
 }
