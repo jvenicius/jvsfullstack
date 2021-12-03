74f2621ad9722c902ee859e0e54bb757f5ae5af7
Venicius <vidivinny8859@gmail.com>
2021-12-03 14:45:11 -0300

    calculadora atualizada

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,58 +1,44 @@
 var painel = document.querySelector(".painel")
+
 var resultado = document.getElementById('resultado');
 var historico = document.getElementById('historico');
-var entrada = document.querySelector("#entrada1");
-// var entrada2 = document.querySelector("#entrada2");
-// var operadores = document.querySelector("#operadores");
+var entrada1 = document.querySelector("#entrada1");
+var entrada2 = document.querySelector("#entrada2");
+var operador = document.querySelector("#operador")
 
 // var operacao = operadores.options[operadores.selectedIndex].text;
 
 const numeros = [0,1,2,3,4,5,6,7,8,9]
 const operadores = ['+','-','*','/']
 
-painel.addEventListener("keypress", function(e){
-    if (numeros.find(element => element == e.key) == undefined && operadores.find(element => element == e.key) == undefined){
+painel.addEventListener("keypress", function(e) {
+    if (numeros.find(element => element == e.key) == undefined) {
         e.preventDefault();
     }
     if (e.key == "," || e.key == ".") {
-        if (entrada.value.indexOf('.') == -1) {
-            entrada.value += '.';
+        if (entrada1.value.indexOf('.') == -1) {
+            entrada1.value += '.';
+        }
+    }
+    if (e.key == "," || e.key == ".") {
+        if (entrada2.value.indexOf('.') == -1) {
+            entrada2.value += '.';
         }
     }
 });
 
+operador.addEventListener("keypress", function(e) {
+    if (operadores.find(element => element == e.key) == undefined) {
+        e.preventDefault();
+    }
+});
+
 function calcular() {
-    var valor = entrada.value;
-    var n = parseFloat(valor);
-
-    // INÍCIO - Cálculos
-    if (valor.indexOf('+') !== undefined) {
-        var n1 = Number(valor.slice(0,valor.indexOf('+')));
-        var n2 = Number(valor.slice(valor.indexOf('+')+1,));
-        var res = n1+n2;
-    }
-    
-    if (valor.indexOf('-') !== undefined) {
-        var n1 = Number(valor.slice(0,valor.indexOf('-')));
-        var n2 = Number(valor.slice(valor.indexOf('-')+1,));
-        var res = n1-n2;
-    }
-
-    if (valor.indexOf('*') !== undefined) {
-        var n1 = Number(valor.slice(0,valor.indexOf('*')));
-        var n2 = Number(valor.slice(valor.indexOf('*')+1,));
-        var res = n1*n2;
-    }
-    
-    if (valor.indexOf('/') !== undefined) {
-        var n1 = Number(valor.slice(0,valor.indexOf('/')));
-        var n2 = Number(valor.slice(valor.indexOf('/')+1,));
-        var res = n1/n2;
-    }
-    // FIM - Cálculos
+    // var valor = entrada.value;
+    // var n = parseFloat(valor);
 
     resultado.classList.add('text-primary');
     resultado.classList.remove('text-muted');
-    resultado.innerHTML = `${res}`;
+    resultado.innerHTML = `Funcionando!`;
     
 }
