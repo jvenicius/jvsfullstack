4c7e47476eb0b7aef5c666ff53fbb91b8dd9267a
Venicius <vidivinny8859@gmail.com>
2021-12-05 14:47:25 -0300

    grandes avancos, calculadora funcionando e salvando historico

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,9 +1,11 @@
 var painel = document.querySelector(".painel")
 
 var resultado = document.getElementById('resultado');
-var historico = document.getElementById('historico');
+
 var entrada1 = document.querySelector("#entrada1");
 var entrada2 = document.querySelector("#entrada2");
+
+var historico = document.querySelector('#historico');
 
 // var operador = document.getElementById("sinal")
 var operadores = document.getElementById("operadores")
@@ -16,7 +18,7 @@ var operadores = document.getElementById("operadores")
 //     };
 // });
 
-operadores.addEventListener("change", calcular());
+operadores.addEventListener("click", calcular());
 
 painel.addEventListener("keypress", function(e) {
     const numeros = [0,1,2,3,4,5,6,7,8,9];
@@ -38,9 +40,50 @@ painel.addEventListener("keypress", function(e) {
 function calcular() {
     // var valor = entrada.value;
     // var n = parseFloat(valor);
+    var n1 = Number(entrada1.value);
+    var n2 = Number(entrada2.value);
     var operacao = operadores.options[operadores.selectedIndex].value;
-    resultado.classList.add('text-primary');
-    resultado.classList.remove('text-muted');
-    resultado.innerHTML = `${operacao} Funcionando!`;
-    
+    if (entrada1.length !==0 && entrada2.length !==0) {
+        let item = document.createElement("option");
+        resultado.classList.add('text-primary');
+        resultado.classList.remove('text-muted');
+        // resultado.innerHTML = `${operacao}, ${n1}, ${n2}`;
+        switch (operacao) {
+            case 'adicao':
+                var res = n1+n2
+                resultado.innerHTML = `${res.toFixed(2)}`
+                item.text = `${n1} + ${n2} = ${res}`
+                item.value = `${n1,n2,res,operacao}`
+                historico.appendChild(item);
+                break
+            case 'multiplicacao':
+                var res = n1*n2
+                resultado.innerHTML = `${res.toFixed(2)}`
+                item.text = `${n1} x ${n2} = ${res}`
+                item.value = `${n1,n2,res,operacao}`
+                historico.appendChild(item);
+                break
+            case 'divisao':
+                var res = n1/n2
+                resultado.innerHTML = `${res.toFixed(2)}`
+                item.text = `${n1} / ${n2} = ${res}`
+                item.value = `${n1,n2,res,operacao}`
+                historico.appendChild(item);
+                break
+            case 'subtracao':
+                var res = n1-n2
+                resultado.innerHTML = `${res.toFixed(2)}`
+                item.text = `${n1} - ${n2} = ${res}`
+                item.value = `${n1,n2,res,operacao}`
+                historico.appendChild(item);
+                break
+        }
+    }
 }
+
+function redefinir() {
+    historico.innerHTML = ``
+    resultado.innerHTML = `Informe valores`
+    resultado.classList.add('text-muted')
+}
+
