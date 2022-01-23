e16253d4c4740c9d3ec480811223b9a600d67ccf
Venicius <vidivinny8859@gmail.com>
2022-01-23 15:25:06 -0300

    v0.0.5 - painel so recebe numeros

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,6 +1,7 @@
-var painel = document.querySelector("#calculator-screen")
+var painel = document.getElementById('calculator-screen')
 
 var operadores = document.getElementById("operadores")
+
 
 // operador.addEventListener("keypress", function(s) {
 //     const sinais = ['+','-','*','/'];
@@ -9,18 +10,16 @@ var operadores = document.getElementById("operadores")
 //     };
 // });
 
-operadores.addEventListener("click", calcular());
-
 painel.addEventListener("keypress", function(e) {
     const numeros = [0,1,2,3,4,5,6,7,8,9];
     if (numeros.find(element => element == e.key) == undefined) {
         e.preventDefault();
     };
     if (e.key == "," || e.key == ".") {
-        if (entrada1.value.indexOf('.') == -1) {
-            entrada1.value += '.';
+        if (painel.value.indexOf('.') == -1) {
+            painel.value += '.';
         };
     };
 });
 
-alert('Ok!')
+
