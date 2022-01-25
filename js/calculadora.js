35a670cc570b2376da76d2b58f32c75ffb931967
Venicius <vidivinny8859@gmail.com>
2022-01-25 12:03:29 -0300

    v0.0.5 - Calculadora programacao inicial funcionando

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,23 +1,40 @@
-var painel = document.getElementById('calculator-screen')
+var display = document.getElementById('calculator-display');
+var buttons = Array.from(document.getElementsByClassName('btn-calc'));
 
-var operadores = document.getElementById("operadores")
+console.log(buttons);
 
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.value) {
+            case 'C':
+                display.value = '';
+                break;
+            case '=':
+                try {
+                    display.value = eval(display.value);
+                }  catch {
+                    display.value = 'Erro!'
+                }
+                break;
+            case '.':
+                if (display.value.indexOf('.') == -1) {
+                    display.value += '.';
+                };
+                break;
+            default:
+                display.value += e.target.value;
+        }
+    });
+});
 
-// operador.addEventListener("keypress", function(s) {
-//     const sinais = ['+','-','*','/'];
-//     if (sinais.find(element => element == s.key) !== undefined) {
-//         operador.value += s.key
-//     };
-// });
-
-painel.addEventListener("keypress", function(e) {
-    const numeros = [0,1,2,3,4,5,6,7,8,9];
+display.addEventListener("keypress", function(e) {
+    const numeros = [0,1,2,3,4,5,6,7,8,9,'/','*','-','+'];
     if (numeros.find(element => element == e.key) == undefined) {
         e.preventDefault();
     };
     if (e.key == "," || e.key == ".") {
-        if (painel.value.indexOf('.') == -1) {
-            painel.value += '.';
+        if (display.value.indexOf('.') == -1) {
+            display.value += '.';
         };
     };
 });
